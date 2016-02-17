4e9968debd358e570e5a7356abf4b94daf45a228
krakdustten <gybelsdylan@gmail.com>
2016-02-17 19:35:19 +0100

    testing

---
+++ ELOASP/ELOASP/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ELOASP.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ELOASP/ELOASP/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELOASP.Startup))]
+namespace ELOASP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
